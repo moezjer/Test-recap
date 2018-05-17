fc2314855911e93689f01efb33bc715bada6a1c0
moezjer <jerianimoez@gmail.com>
2018-05-17 13:24:46 +0100

    creation fichier d'extension swp

--- script.sh
+++ script.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
 echo "Mise a jour systeme d'exploitation"
-vlc
+echo $1
+pwd
+vlcs $1
+set -e
+echo $?
+echo $?
+echo "Moez"
+if $? -eq "0" 
+then
+echo "le nom du fichier est faux"
+fi
